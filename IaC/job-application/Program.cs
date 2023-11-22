cba01b161c9d48df01f0c202ad3e2566a26212e4
thepowercare <126682621+thepowercare@users.noreply.github.com>
2023-11-22 12:36:27 +0100

    Added template function

--- IaC/job-application/Program.cs
+++ IaC/job-application/Program.cs
@@ -5,8 +5,20 @@ using System.Collections.Generic;
 
 return await Deployment.RunAsync(() =>
 {
-    // Create an Azure Resource Group
-    var resourceGroup = new ResourceGroup("resourceGroup");
+    var azureConfig = new Pulumi.Config("azure"); // Pulumi.{stack}.yaml
+    var azLocation = azureConfig.Require("location"); 
+    var resourceType = "rg";
+    var application = "heiselberg-contact-form";
+
+    var environment = Pulumi.Deployment.Instance.StackName;
+
+    var instanceCount = "001";
+
+    // Create an Azure Resource Group {resource-type}-{application}-{environment}-{az-region}-{instance-count}
+    // {rg}-{heiselberg-contact-form}-{dev}-{northeurope}-{001}-{pulumi-id(AUTO)}
+    var resourceGroupName = $"{resourceType}-{application}-{environment}-{azRegion}-{instanceCount}-";
+
+    var resourceGroup = new Pulumi.Azure.Core.ResourceGroup(resourceGroupName, new() { Location = azRegion }); 
 
     // Create an Azure Storage Account
     var storageAccount = new Account("storage", new AccountArgs
---
+++ README.md
@@ -0,0 +1,31 @@
+
+# About
+
+The purpose of this project is to give the foundation to create, deploy and maintain a super simple contact form site - as cheap and easy as possible. While still applying good programming and ci-cd practices.
+
+## Building blocks
+
+``` plantuml
+
+* Git
+* .NET c#
+* Docker
+* dev container
+* Services
+** Azure
+***_ App Function
+** MailChimp
+***_ Free (1000 mails/month)
+** Github
+*** Github actions
+* Build
+**_ Nuke Build
+* IaC
+**_ Pulumi
+* Documentation
+**_ MD
+**_ Plantuml
+
+```
+
+# Getting started
