40353dcc601d06a4b7b381de864ff329afa0fa84
thepowercare <126682621+thepowercare@users.noreply.github.com>
2023-12-21 09:27:28 +0100

    Minimal startup created

---
+++ Design/_doc_Design.md
@@ -0,0 +1,12 @@
+
+# Design
+
+## Logo
+
+Background:
+Name: DarkSlateGray
+Hex: #012f49
+
+Surname:
+Name: Aqua
+Hex: #00ffff
---
+++ src/Frontend-elm/src/Main.elm
@@ -0,0 +1,4 @@
+module Main exposing (..)
+import Html
+main : Html.Html msg
+main = Html.text "Let's get this party started!"
